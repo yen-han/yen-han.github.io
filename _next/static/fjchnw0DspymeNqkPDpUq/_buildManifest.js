b712152378b5db22a835ef39f019257713777be0
yen-han <yenhan.dev@gmail.com>
2023-01-05 15:45:27 +0000

    Deploying to gh-pages from @ yen-han/yen-han.github.io@63d9ae4558f0dc6705a23573d54bcb35b06b3360 🚀

--- _next/static/fjchnw0DspymeNqkPDpUq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/464da46a22063e3f.css","static/chunks/pages/index-425d5eebda1a6b31.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
