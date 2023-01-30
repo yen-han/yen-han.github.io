b611934477972ff41ef43d0d00842c47c988a45a
Yeaeun Han <yenhan.dev@gmail.com>
2023-01-30 15:57:32 -0500

    feat: add animation on contact reference

--- components/ContactRef/ContactRef.tsx
+++ components/ContactRef/ContactRef.tsx
@@ -1,29 +1,56 @@
+import { useEffect } from "react";
+import { gsap } from "gsap";
 import styles from "./ContactRef.module.scss";
 
 function ContactRef() {
+  useEffect(() => {
+    gsap.fromTo(
+      ".icon",
+      {
+        y: 70,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1.5,
+        stagger: 0.2,
+        ease: "power3.out",
+      }
+    );
+  }, []);
+
   return (
     <section className={styles.ContactRef}>
       <div className={styles.reference}>
-        <a
-          href="https://github.com/yen-han"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <i className="bi bi-github"></i>
-        </a>
-        <a
-          href="https://www.linkedin.com/in/yen-han/"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <i className="bi bi-linkedin"></i>
-        </a>
-        <a href="assets/YenHan_Resume.pdf" download>
-          <i className="bi bi-file-earmark-person-fill"></i>
-        </a>
-        <a href="mailto:yenhan.dev@gmail.com">
-          <i className="bi bi-envelope-fill"></i>
-        </a>
+        <div className={"icon"}>
+          <a
+            href="https://github.com/yen-han"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <i className={"bi bi-github"}></i>
+          </a>
+        </div>
+        <div className={"icon"}>
+          <a
+            href="https://www.linkedin.com/in/yen-han/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <i className={"bi bi-linkedin"}></i>{" "}
+          </a>
+        </div>
+        <div className={"icon"}>
+          <a href="assets/YenHan_Resume.pdf" download>
+            <i className="bi bi-file-earmark-person-fill"></i>
+          </a>
+        </div>
+        <div className={"icon"}>
+          <a href="mailto:yenhan.dev@gmail.com">
+            <i className="bi bi-envelope-fill"></i>
+          </a>
+        </div>
       </div>
     </section>
   );
